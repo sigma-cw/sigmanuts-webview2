07d795da37aa976df7122151d729fa5376c4e12b
Chroneco <chroneco2@gmail.com>
2023-02-12 17:40:27 +0700

    Add raiseMembershipGiftEvent in callback Add raiseMembershipRedemptionEvent in callback - Have not been parsed from html Add timeout for sticker basic event
    Disable cache deletion in MainWindow.cs
    
    Add reconnect event
    Add clear message on reconnect
    Update reconnect retry time
    - under 10 seconds: 5s
    - under 5 minutes: 12s
    - under 15 minutes: 60s
    - over 15 minutes: stop retry
    Fix bug
    
    Update background and font of Youtube

--- web-src/widgets/YouTube/basic.js
+++ web-src/widgets/YouTube/basic.js
@@ -1,44 +1,59 @@
+$(document).ready(function () {
 
-window.addEventListener('onEventReceived', function (obj) {
-    //console.log(obj.detail);
+    window.addEventListener('onEventReceived', function (obj) {
+        //console.log(obj.detail);
 
-    if (obj.detail.listener == "url-change") {
-        clearMessages();
-        return;
+        if (obj.detail.listener == "url-change") {
+            clearMessages();
+            return;
+        }
+
+        if (obj.detail.listener == "reconnect") {
+            console.log("Chat reconnected");
+            clearMessages();
+            return;
+        }
+
+        if (obj.detail.listener == "delete-message") {
+            let msgId = obj.detail.event.data.msgId;
+            removeMessage(msgId);
+            return;
+        }
+
+        if (obj.detail.listener !== "youtube-basic") return;
+
+        let htmlText = obj.detail.event.html;
+        addElement(htmlText);
+    });
+
+    window.addEventListener('onWidgetLoad', function (obj) {
+
+    });
+
+    function clearMessages() {
+        $('yt-live-chat-item-list-renderer div#items').empty();
     }
 
-    if (obj.detail.listener == "delete-message") {
-        let msgId = obj.detail.event.data.msgId;
-        removeMessage(msgId);
-        return;
+    function removeMessage(msgId) {
+        if (!msgId) return;
+
+        //Some messages have %3D at the end of the id, breaking the code
+        msgId = msgId.replaceAll(`%3D`, "");
+        let elem = $(`#${msgId}`);
+        if (elem) {
+            elem.remove();
+        }
     }
 
-    if (obj.detail.listener !== "youtube-basic") return;
+    function addElement(htmlText) {
+        if (!htmlText) return;
+        element = $.parseHTML(htmlText);
 
-    let htmlText = obj.detail.event.html;
-    addElement(htmlText);
+        //Some messages have %3D at the end of the id, breaking the code
+        let id = $(element).attr("id");
+        id = id.replaceAll(`%3D`, "");
+        $(element).attr("id", id);
+
+        $(element).appendTo('yt-live-chat-item-list-renderer div#items');
+    }
 });
-
-function clearMessages() {
-    $('yt-live-chat-item-list-renderer div#items').empty();
-}
-
-function removeMessage(msgId) {
-    //Some messages have %3D at the end of the id, breaking the code
-    msgId = msgId.replaceAll(`%3D`, "");
-    let elem = $(`#${msgId}`);
-    if (elem) {
-        elem.remove();
-    }
-}
-
-function addElement(htmlText) {
-    element = $.parseHTML(htmlText);
-
-    //Some messages have %3D at the end of the id, breaking the code
-    let id = $(element).attr("id");
-    id = id.replaceAll(`%3D`, "");
-    $(element).attr("id", id);
-
-    $(element).appendTo('yt-live-chat-item-list-renderer div#items');
-}
